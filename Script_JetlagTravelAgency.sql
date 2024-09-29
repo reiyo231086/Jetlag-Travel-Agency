33a2aa384374071a35fe1c7c2674689c9ae77b37
Reinaldo Orellana <145404154+reiyo231086@users.noreply.github.com>
2024-09-28 22:43:58 -0300

    segunda preentrega

--- Script_JetlagTravelAgency.sql
+++ Script_JetlagTravelAgency.sql
@@ -1,67 +1,91 @@
-CREATE DATABASE  IF NOT EXISTS `jetlag_travel_agency` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+DROP DATABASE `jetlag_travel_agency`;
+CREATE DATABASE  IF NOT EXISTS `jetlag_travel_agency`;
 USE `jetlag_travel_agency`;
--- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
---
--- Host: localhost    Database: jetlag_travel_agency
--- ------------------------------------------------------
--- Server version	8.0.39
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `cliente`
---
 
 DROP TABLE IF EXISTS `cliente`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cliente` (
   `id_cliente` int NOT NULL AUTO_INCREMENT,
   `dni` varchar(50) DEFAULT NULL,
   `nombre` varchar(50) DEFAULT NULL,
   `apellido` varchar(50) DEFAULT NULL,
   `mail` varchar(50) DEFAULT NULL,
-  `telefono` int DEFAULT NULL,
-  `creado` datetime DEFAULT NULL,
+  `telefono` varchar (20) DEFAULT NULL,
+  `creado` DATE DEFAULT NULL,
   PRIMARY KEY (`id_cliente`),
   UNIQUE KEY `dni` (`dni`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Table structure for table `compra`
---
+DROP TABLE IF EXISTS `sucursal`;
+CREATE TABLE `sucursal` (
+  `id_sucursal` int NOT NULL AUTO_INCREMENT,
+  `nombre_sucursal` varchar(100) DEFAULT NULL,
+  `telefono_sucursal` varchar(20) DEFAULT NULL,
+  `direccion_sucursal` varchar(255) DEFAULT NULL,
+  `mail` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_sucursal`)
+);
 
-DROP TABLE IF EXISTS `compra`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `compra` (
-  `id_compra` int NOT NULL AUTO_INCREMENT,
+-- AGREGO TABLA VENDEDOR 
+DROP TABLE IF EXISTS `vendedor`;
+
+CREATE TABLE `vendedor` (
+  `nro_legajo` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellido` varchar(50) DEFAULT NULL,
+  `mail` varchar(50) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `cargo` varchar(50) DEFAULT NULL,
+  `id_sucursal` int NOT NULL,
+  PRIMARY KEY (`nro_legajo`),
+  FOREIGN KEY (`id_sucursal`) REFERENCES `sucursal` (`id_sucursal`)
+);
+
+-- AGREGO TABLA PROVEEDOR 
+
+DROP TABLE IF EXISTS `proveedor`;
+CREATE TABLE `proveedor` (
+  `id_proveedor` int NOT NULL AUTO_INCREMENT,
+  `cuit` varchar(20) DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `mail` varchar(100) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_proveedor`)
+) AUTO_INCREMENT = 5000;
+
+-- CREO TABLA RESERVA 
+DROP TABLE IF EXISTS `reserva`;
+CREATE TABLE `reserva` (
+  `id_reserva` int NOT NULL AUTO_INCREMENT,
+  `id_cliente` int DEFAULT NULL,
   `id_pasaje` int DEFAULT NULL,
-  `fecha_compra` date DEFAULT NULL,
-  `tipo_compra` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id_compra`),
-  KEY `fk_id_pasaje` (`id_pasaje`),
-  CONSTRAINT `fk_id_pasaje` FOREIGN KEY (`id_pasaje`) REFERENCES `pasaje` (`id_pasaje`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `monto_total` decimal(10,2) DEFAULT NULL,
+  `monto_abonado` decimal(10,2) DEFAULT '0.00',
+  `fecha_reserva` date DEFAULT NULL,
+  `tipo_reserva` varchar(50) DEFAULT NULL,
+  `estado` enum('Pendiente de Pago','Seña Pagada','Total Pagado','Confirmado','Cancelado') NOT NULL,
+  PRIMARY KEY (`id_reserva`),
+  KEY `id_cliente` (`id_cliente`),
+  CONSTRAINT `reserva_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`)
+);
 
---
--- Table structure for table `pasaje`
---
+-- INSERTO TABLA VENTAS 
+DROP TABLE IF EXISTS `venta`;
+CREATE TABLE `venta` (
+  `id_venta` int NOT NULL AUTO_INCREMENT,
+  `id_reserva` int DEFAULT NULL,
+  `nro_legajo` int DEFAULT NULL,
+  `monto_pago` decimal(10,2) DEFAULT NULL,
+  `fecha_pago` datetime DEFAULT NULL,
+  `forma_pago` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_venta`),
+  KEY `id_reserva` (`id_reserva`),
+  KEY `fk_nro_legajo` (`nro_legajo`),
+  CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`id_reserva`) REFERENCES `reserva` (`id_reserva`)
+);ALTER TABLE venta AUTO_INCREMENT = 36;TRUNCATE TABLE venta;
 
+-- CREO TABAL PASAJES 
 DROP TABLE IF EXISTS `pasaje`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pasaje` (
   `id_pasaje` int NOT NULL AUTO_INCREMENT,
   `id_venta` int DEFAULT NULL,
@@ -78,123 +102,41 @@ CREATE TABLE `pasaje` (
   KEY `id_reserva` (`id_reserva`),
   KEY `id_proveedor` (`id_proveedor`),
   CONSTRAINT `pasaje_ibfk_1` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id_venta`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Table structure for table `proveedor`
---
+-- INSERTO TABLA COMPRA 
 
-DROP TABLE IF EXISTS `proveedor`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `proveedor` (
-  `id_proveedor` int NOT NULL AUTO_INCREMENT,
-  `id_compra` int DEFAULT NULL,
-  `cuit` varchar(20) DEFAULT NULL,
-  `direccion` varchar(255) DEFAULT NULL,
-  `mail` varchar(100) DEFAULT NULL,
-  `telefono` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id_proveedor`),
-  KEY `id_compra` (`id_compra`),
-  CONSTRAINT `proveedor_ibfk_1` FOREIGN KEY (`id_compra`) REFERENCES `compra` (`id_compra`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `reserva`
---
-
-DROP TABLE IF EXISTS `reserva`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `reserva` (
-  `id_reserva` int NOT NULL AUTO_INCREMENT,
-  `id_cliente` int DEFAULT NULL,
+DROP TABLE IF EXISTS `compra`;
+CREATE TABLE `compra` (
+  `id_compra` int NOT NULL AUTO_INCREMENT,
   `id_pasaje` int DEFAULT NULL,
-  `monto_total` decimal(10,2) DEFAULT NULL,
-  `monto_abonado` decimal(10,2) DEFAULT '0.00',
-  `fecha_reserva` date DEFAULT NULL,
-  `tipo_reserva` varchar(50) DEFAULT NULL,
-  `estado` enum('Pendiente de Pago','Seña Pagada','Total Pagado','Confirmado','Cancelado') NOT NULL,
-  PRIMARY KEY (`id_reserva`),
-  KEY `id_cliente` (`id_cliente`),
-  CONSTRAINT `reserva_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `sucursal`
---
-
-DROP TABLE IF EXISTS `sucursal`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `sucursal` (
-  `id_sucursal` int NOT NULL AUTO_INCREMENT,
+  `fecha_compra` date DEFAULT NULL,
+  `tipo_compra` varchar(50) DEFAULT NULL,
+  `id_proveedor` int DEFAULT NULL,
   `nro_legajo` int DEFAULT NULL,
-  `nombre_sucursal` varchar(100) DEFAULT NULL,
-  `telefono_sucursal` varchar(20) DEFAULT NULL,
-  `direccion_sucursal` varchar(255) DEFAULT NULL,
-  `mail` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id_sucursal`),
-  KEY `nro_legajo` (`nro_legajo`),
-  CONSTRAINT `sucursal_ibfk_1` FOREIGN KEY (`nro_legajo`) REFERENCES `vendedor` (`nro_legajo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `vendedor`
---
-
-DROP TABLE IF EXISTS `vendedor`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `vendedor` (
-  `nro_legajo` int NOT NULL AUTO_INCREMENT,
-  `id_venta` int DEFAULT NULL,
-  `id_compra` int DEFAULT NULL,
-  `nombre` varchar(50) DEFAULT NULL,
-  `apellido` varchar(50) DEFAULT NULL,
-  `mail` varchar(50) DEFAULT NULL,
-  `telefono` varchar(20) DEFAULT NULL,
-  `cargo` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`nro_legajo`),
-  KEY `id_venta` (`id_venta`),
-  KEY `fk_id_compra` (`id_compra`),
-  CONSTRAINT `fk_id_compra` FOREIGN KEY (`id_compra`) REFERENCES `compra` (`id_compra`),
-  CONSTRAINT `vendedor_ibfk_1` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id_venta`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `venta`
---
-
-DROP TABLE IF EXISTS `venta`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `venta` (
-  `id_venta` int NOT NULL AUTO_INCREMENT,
-  `id_reserva` int DEFAULT NULL,
-  `nro_legajo` int DEFAULT NULL,
-  `monto_pago` decimal(10,2) DEFAULT NULL,
-  `fecha_pago` datetime DEFAULT NULL,
-  `tipo_venta` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id_venta`),
-  KEY `id_reserva` (`id_reserva`),
+  PRIMARY KEY (`id_compra`),
+  KEY `fk_id_pasaje` (`id_pasaje`),
+  KEY `fk_id_proveedor` (`id_proveedor`),
   KEY `fk_nro_legajo` (`nro_legajo`),
-  CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`id_reserva`) REFERENCES `reserva` (`id_reserva`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+  CONSTRAINT `fk_id_pasaje` FOREIGN KEY (`id_pasaje`) REFERENCES `pasaje` (`id_pasaje`),
+  CONSTRAINT `fk_id_proveedor` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedor` (`id_proveedor`),
+  CONSTRAINT `fk_nro_legajo` FOREIGN KEY (`nro_legajo`) REFERENCES `vendedor` (`nro_legajo`)  
+);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+DROP TABLE IF EXISTS `compra`;
+CREATE TABLE `compra` (
+  `id_compra` int NOT NULL AUTO_INCREMENT,
+  `id_pasaje` int DEFAULT NULL,
+  `fecha_compra` date DEFAULT NULL,
+  `tipo_compra` varchar(50) DEFAULT NULL,
+  `id_proveedor` int DEFAULT NULL,
+  `nro_legajo` int DEFAULT NULL,
+  PRIMARY KEY (`id_compra`),
+  KEY `fk_id_pasaje` (`id_pasaje`),
+  KEY `fk_id_proveedor` (`id_proveedor`),
+  KEY `fk_nro_legajo` (`nro_legajo`),
+  CONSTRAINT `fk_id_pasaje` FOREIGN KEY (`id_pasaje`) REFERENCES `pasaje` (`id_pasaje`),
+  CONSTRAINT `fk_id_proveedor` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedor` (`id_proveedor`),
+  CONSTRAINT `fk_nro_legajo` FOREIGN KEY (`nro_legajo`) REFERENCES `vendedor` (`nro_legajo`)  
+);
 
--- Dump completed on 2024-09-09 13:04:42
